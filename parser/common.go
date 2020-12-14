26488a0fc67df65d17aa6f25a2b9ed133d014772
Seif Lotfy <seif.lotfy@gmail.com>
2020-12-14 12:47:42 +0000

    set default json depth to 3

--- parser/common.go
+++ parser/common.go
@@ -16,10 +16,31 @@ func MatchUnknownMessage(msg string) (map[string]interface{}, string) {
 	if err := json.Unmarshal([]byte(msg), &dict); err != nil {
 		return NewRawMessage(msg)
 	}
-	return dict, FormatJSON
+	data := shrinkJSON(dict, 3)
+	return data, FormatJSON
 }
 
 // NewRawMessage ...
 func NewRawMessage(msg string) (map[string]interface{}, string) {
 	return map[string]interface{}{"message": msg}, FormatUnknown
+}
+
+// TrimJSON ...
+func shrinkJSON(data map[string]interface{}, depth uint8) map[string]interface{} {
+	newData := make(map[string]interface{})
+	for k, v := range data {
+		if depth <= 1 {
+			p, _ := json.Marshal(v)
+			newData[k] = string(p)
+			continue
+		}
+
+		subData, ok := v.(map[string]interface{})
+		if !ok {
+			newData[k] = v
+		} else {
+			newData[k] = shrinkJSON(subData, depth-1)
+		}
+	}
+	return newData
 }
